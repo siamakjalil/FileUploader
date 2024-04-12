de7e9b7efb31838f96c229eddc5c79950b13c2ea
Saeed <saeed.izadi8401@gmail.com>
2024-04-12 18:01:28 +0330

    Update FileDirectory.cs

--- FileUploader/FileDirectory.cs
+++ FileUploader/FileDirectory.cs
@@ -7,16 +7,7 @@ namespace FileUploader
 {
     public static class FileDirectory
     {
-        public static bool Exist(string path)
-        {
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-                return false;
-            }
-            return true;
-        }
-        public static bool Create(string path)
+        public static bool CheckExistsAndCreate(string path)
         {
             try
             {
