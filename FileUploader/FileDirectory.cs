5767c669f52d8b612cb57d02c1714241992ce134
Saeed <saeed.izadi8401@gmail.com>
2024-04-12 17:47:11 +0330

    Create FileDirectory.cs

---
+++ FileUploader/FileDirectory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileUploader
+{
+    public static class FileDirectory
+    {
+        public static bool Exist(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+                return false;
+            }
+            return true;
+        }
+        public static bool Create(string path)
+        {
+            try
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
